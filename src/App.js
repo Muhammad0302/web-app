32d56e98bf46dd7ddf265589744cea273cc42203
Waqar Rashid <waqarrashid33@gmail.com>
2022-10-20 22:39:11 +0200

    fixed bug in handling of incoming data

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const server_url = "http://172.104.156.241:3001";
 
 function App() {
   const [status1, setStatus1] = useState(true)
-  const [status2, setStatus2] = useState(false)
+  const [status2, setStatus2] = useState(true)
   const [status3, setStatus3] = useState(true)
   const socket = io.connect(server_url)
 
@@ -21,37 +21,36 @@ function App() {
   useEffect(() => {
     socket.on("status_update", (data) => {
       const { sensor_id, sensor_status } = data
-      const status = sensor_status !== "false"
 
       if (sensor_id === "prototype_01") {
-        setStatus1(status)
+        setStatus1(sensor_status)
       }
       else if (sensor_id === "prototype_02") {
-        setStatus2(status)
+        setStatus2(sensor_status)
       }
       else if (sensor_id === "prototype_03") {
-        setStatus3(status)
+        setStatus3(sensor_status)
       } else {
         console.log("unexpected sensor id")
       }
     });
     // client-side
     socket.on("connect", () => {
-      console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+      console.log("Connected" + socket.id); // x8WIv7-mJelg7on_ALbx
     });
 
     socket.on("disconnect", () => {
-      console.log(socket.id); // undefined
+      console.log("Disconnected" + socket.id); // undefined
     });
   }, [socket]);
 
   return (
     <div>
       <div>
-        <h1> Status</h1>
-        <Status status={status1} />
-        <Status status={status2} />
-        <Status status={status3} />
+        <h1> Statuses</h1>
+        <Status status={status1} name="Sensor 1" />
+        <Status status={status2} name="Sensor 2" />
+        <Status status={status3} name="Sensor 3" />
       </div>
     </div>
   );
