941cfb894a8de44507b817077ab2920f204eb506
maaz khan <maazkhaansofteng@gmail.com>
2022-11-29 02:13:00 -0800

    bugs fixed

--- src/components/Mymap.js
+++ src/components/Mymap.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-
+import PropTypes from 'prop-types'
 import "./status.css";
-export const Mymap = () => {
+export const Mymap = ({location, status}) => {
   return (
     // to run it without error remove location and status and instead of location put [45.4, -75.7]
-    <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
+    <MapContainer center={location} zoom={12} scrollWheelZoom={false}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[45.4, -75.7]}>
+      <Marker position={location}>
         <Popup>
-          status  <br />true.
+          status  <br />{status}.
         </Popup>
       </Marker>
     </MapContainer>
   );
 };
+Mymap.prototype={
+  location:PropTypes.number,
+  status:PropTypes.bool
+}
+Mymap.defaultProps = {
+  location: [45.4, -75.7],
+  status: true
+}
