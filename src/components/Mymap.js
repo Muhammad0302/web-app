1a63fe94cf663d6d7e63e12d0edec2628213ec7e
maaz khan <maazkhaansofteng@gmail.com>
2022-11-30 23:06:07 -0800

    two map marker added red and green

--- src/components/Mymap.js
+++ src/components/Mymap.js
@@ -2,7 +2,24 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import propTypes from 'prop-types'
 import "./status.css";
+import * as L from "leaflet";
+// import { Icon } from "leaflet";
+// import { iconPerson } from "./icons";
 export const Mymap = ({ location, mstatus }) => {
+  const LeafIcon = L.Icon.extend({
+    options: {},
+  });
+  const redIcon = new LeafIcon({
+    iconUrl:
+      "http://maps.google.com/mapfiles/kml/paddle/stop.png",
+  });
+  const greenIcon = new LeafIcon({
+    iconUrl:
+      "http://maps.google.com/mapfiles/kml/paddle/grn-square.png",
+  });
+
+
+ 
   return (
     // to run it without error remove location and status and instead of location put [45.4, -75.7]
     <MapContainer center={location} zoom={12} scrollWheelZoom={false}>
@@ -10,7 +27,8 @@ export const Mymap = ({ location, mstatus }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={location}>
+      
+      <Marker  position={location} icon={mstatus=="true" ? redIcon : greenIcon}>
         <Popup>
           <p>status  {mstatus}.</p>
         </Popup>
@@ -26,5 +44,5 @@ Mymap.propTypes={
 // defaul values for props
 Mymap.defaultProps = {
   location: [45.4, -75.7],// default location 
-  mstatus: "default-true"  // this supposed to boolean
+  mstatus: "false"  // this supposed to boolean
 }
