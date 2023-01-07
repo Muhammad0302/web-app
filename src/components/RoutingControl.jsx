0d6e1b6f67753cba54168ffbb6044c1adb346693
maaz khan <maazkhaansofteng@gmail.com>
2023-01-07 19:37:22 +0500

    bugs

--- src/components/RoutingControl.jsx
+++ src/components/RoutingControl.jsx
@@ -1,16 +1,20 @@
 // this component is for to set the routs from one marker t other
+import { useRef } from 'react';
 import L from 'leaflet';
 import { createControlComponent } from '@react-leaflet/core';
 import 'leaflet-routing-machine';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 
 const createRoutineMachineLayer = ({ position, start, end, color }) => {
-  const instance = L.Routing.control({
+  const RoutingMachineRef = useRef(null);
+  RoutingMachineRef.current = L.Routing.control({
+    
     position,
     waypoints: [
       start,
       end
     ],
+    
     lineOptions: {
       styles: [
         {
@@ -18,10 +22,10 @@ const createRoutineMachineLayer = ({ position, start, end, color }) => {
           weight: 8,
         },
       ],
+     
     },
   });
-
-  return instance;
+  return RoutingMachineRef.current;
 };
 
 const RoutingMachine = createControlComponent(createRoutineMachineLayer);
