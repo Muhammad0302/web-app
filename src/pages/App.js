ebffce805cd867139023c987563d62d62051acc0
Waqar Rashid <waqarrashid33@gmail.com>
2023-03-26 12:49:55 +0200

    Fixed linter warnings

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import MyMap from '../components/MyMap';
-import Header from '../components/Header'
 
 function App() {
   return (
