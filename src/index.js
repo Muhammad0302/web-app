1ebffbb3df722cc68cdf410f62e9c8d97402dbaf
immaazkhan <117020351+immaazkhan@users.noreply.github.com>
2023-04-01 15:09:15 +0500

    separated map from leds and update header (#31)
    * separated map from leds and update header
    * delete distance.jsx
    ---------
    
    Co-authored-by: Waqar Rashid <waqarrashid33@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './pages/App';
-import LEDs from './pages/LEDs';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
@@ -13,7 +12,6 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/web-app" element={<App />} />
-      <Route path="/web-app/leds" element={<LEDs />} />
     </Routes>
   </BrowserRouter>
 );
