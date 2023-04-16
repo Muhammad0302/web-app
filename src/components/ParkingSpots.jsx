e4131922872ffeca7646bead9ac4f52ca76af862
immaazkhan <117020351+immaazkhan@users.noreply.github.com>
2023-04-16 19:10:00 +0500

    Removing socket (#37)
    * removed socket from json and code
    
    * removed leds component

--- src/components/ParkingSpots.jsx
+++ src/components/ParkingSpots.jsx
@@ -1,9 +1,7 @@
 import * as L from 'leaflet';
 import React, { useEffect, useState } from 'react';
 import { Marker } from 'react-leaflet';
-import _ from 'lodash';
-import io from 'socket.io-client';
-import { __API_HOST__ } from '../constants'
+import { __API_HOST__ } from '../constants';
 
 export default function ParkingSpots({ setTarget, target }) {
   const LeafIcon = L.Icon.extend({ options: {} });
@@ -19,7 +17,6 @@ export default function ParkingSpots({ setTarget, target }) {
   });
 
   const [sensors, setSensors] = useState(null)
-  const socket = io.connect(__API_HOST__)
 
   useEffect(() => {
     fetch(`${__API_HOST__}/sensor/getAllSensors`)
@@ -30,34 +27,12 @@ export default function ParkingSpots({ setTarget, target }) {
         throw response
       })
       .then(data => {
-        console.log(data)
         setSensors(data)
       })
+      .catch(error => {
+        console.error('Error retrieving sensor data:', error);
+      });
   }, [])
-
-  useEffect(() => {
-    // {location: {lat: 52.500772, lng: 13.472764}, sensorId: "sensor_01", status: true}
-    socket.on('v001_status_update', (data) => {
-      const { sensorId, status } = data
-
-      if (sensors) {
-        const newSensors = _.cloneDeep(sensors)
-        const index = newSensors.findIndex(sensor => sensor.sensorId == sensorId)
-        if (newSensors[index].status != status) {
-          newSensors[index].status = status
-          setSensors(newSensors)
-        }
-      }
-    });
-
-    socket.on('connect', () => {
-      console.log(`Connected${socket.id}`);
-    });
-
-    socket.on('disconnect', () => {
-      console.log(`Disconnected${socket.id}`); // undefined
-    });
-  }, [socket]);
 
   return sensors && sensors.map(sensor => {
     const { sensorId, location, status } = sensor;
