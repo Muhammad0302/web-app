f1868db00e90c50b89b2adf25e1e6f245bed78d6
@mi477048 <mi477048@gmail.com>
2023-04-19 00:50:50 +0500

    perodically sensors fetching added

--- src/components/ParkingSpots.jsx
+++ src/components/ParkingSpots.jsx
@@ -8,52 +8,60 @@ export default function ParkingSpots({ setTarget, target }) {
   const redIcon = new LeafIcon({
     iconUrl: 'https://maps.google.com/mapfiles/kml/paddle/stop.png',
     iconSize: [64, 64],
-    iconAnchor: [32, 64],
+    iconAnchor: [32, 64]
   });
   const greenIcon = new LeafIcon({
     iconUrl: 'https://maps.google.com/mapfiles/kml/paddle/grn-square.png',
     iconSize: [64, 64],
-    iconAnchor: [32, 64],
+    iconAnchor: [32, 64]
   });
 
-  const [sensors, setSensors] = useState(null)
+  const [sensors, setSensors] = useState(null);
 
   useEffect(() => {
-    fetch(`${__API_HOST__}/sensor/getAllSensors`)
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        }
-        throw response
-      })
-      .then(data => {
-        setSensors(data)
-      })
-      .catch(error => {
-        console.error('Error retrieving sensor data:', error);
-      });
-  }, [])
+    const interval = setInterval(() => {
+      console.log('Get all sensor api called after 10 seconds');
+      fetch(`${__API_HOST__}/sensor/getAllSensors`)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw response;
+        })
+        .then(data => {
+          setSensors(data);
+        })
+        .catch(error => {
+          console.error('Error retrieving sensor data:', error);
+        });
+    }, 10000);
 
-  return sensors && sensors.map(sensor => {
-    const { sensorId, location, status } = sensor;
-    
-    return (
-      <Marker
-        key={sensorId}
-        position={[location.coordinates[0], location.coordinates[1]]}
-        icon={status == 'inactive' ? redIcon : greenIcon}
-        eventHandlers={{
-          click: () => {
-            if (target) {
-              console.log(`SensorId ${sensorId}`);
-              setTarget(null);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    sensors &&
+    sensors.map(sensor => {
+      const { sensorId, location, status } = sensor;
+
+      return (
+        <Marker
+          key={sensorId}
+          position={[location.coordinates[0], location.coordinates[1]]}
+          icon={status == 'inactive' ? redIcon : greenIcon}
+          eventHandlers={{
+            click: () => {
+              if (target) {
+                console.log(`SensorId ${sensorId}`);
+                setTarget(null);
+              }
+            },
+            dblclick: e => {
+              setTarget(e.latlng);
             }
-          },
-          dblclick: (e) => {
-            setTarget(e.latlng);
-          },
-        }}
-      />
-    );
-  });
+          }}
+        />
+      );
+    })
+  );
 }
